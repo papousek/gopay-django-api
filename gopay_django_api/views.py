0e49105d6964955028b258b7f883a52dccfc2601
Jan Papousek <jan.papousek@gmail.com>
2016-10-10 13:23:07 +0200

    run notify in atomic transaction

--- gopay_django_api/views.py
+++ gopay_django_api/views.py
@@ -1,8 +1,10 @@
 from .models import Payment
-from django.shortcuts import get_object_or_404
+from django.db import transaction
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 
+@transaction.atomic()
 def notify(request):
     payment_id = request.GET['id']
     payment = get_object_or_404(Payment, gopay_id=payment_id)
