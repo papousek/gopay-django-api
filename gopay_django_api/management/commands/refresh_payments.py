bcdbe1ce53d644613c26d356642ff089fa054e8e
Jan Papousek <jan.papousek@gmail.com>
2016-11-11 07:43:27 +0100

    run in transaction

--- gopay_django_api/management/commands/refresh_payments.py
+++ gopay_django_api/management/commands/refresh_payments.py
@@ -1,7 +1,8 @@
-from optparse import make_option
-from django.core.management.base import BaseCommand
-from gopay_django_api.models import Payment
 from clint.textui import progress
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from gopay_django_api.models import Payment
+from optparse import make_option
 
 
 class Command(BaseCommand):
@@ -21,7 +22,8 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        for payment in progress.bar(Payment.objects.all()):
-            if options['state'] is not None and payment.state != options['state']:
-                continue
-            Payment.objects.refresh_status(payment, force=options['force'])
+        with transaction.atomic():
+            for payment in progress.bar(Payment.objects.all()):
+                if options['state'] is not None and payment.state != options['state']:
+                    continue
+                Payment.objects.refresh_status(payment, force=options['force'])
